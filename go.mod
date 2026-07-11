7d8fa9e5988757e1b3937d6d65aaace1149fbaac
cristhianGigpz <cjacevedot@gmail.com>
2026-07-11 09:05:09 -0500

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go-patron
+
+go 1.26.2
---
+++ internal/entity/user.go
@@ -0,0 +1,10 @@
+package entity
+
+type User struct {
+	ID       uint
+	Name     string
+	Email    string
+	Age      int
+	Password string
+	Role     string
+}
